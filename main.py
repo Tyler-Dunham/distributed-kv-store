645874e011f5e801c6ef5eb8d1d45df48da9bf1b
Tyler Dunham <tylerjdunham1@gmail.com>
2026-05-14 17:14:48 -0400

    start kv stuff

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from raft import Raft
+from raft import Redis, Command, Op, Role
 import time
 
 if __name__ == "__main__":
-    raft = Raft()
-    time.sleep(1)
-    for node in raft.nodes:
-        print(node.role, node.term)
+    redis = Redis()
+    time.sleep(0.5)
+    for node in redis.nodes:
+        print(node.role, node.kv)
+
+    command = Command(Op.GET, 1, "Tyler")
+    redis.send(command)
+
+    for node in redis.nodes:
+        if node.role == Role.LEADER:
+            print(node.log)
     
--- raft.py
+++ raft.py
@@ -3,28 +3,63 @@ import threading
 import random
 import time
 
-class Raft:
+class Command:
+    def __init__(self, op, key, value=None):
+        self.op = op
+        self.key = key
+        self.value = value
+        self.status = LogStatus.APPENDED
+
+    def __repr__(self):
+        return (
+            f"Command(op={self.op!r}, "
+            f"key={self.key!r}, "
+            f"value={self.value!r}), "
+            f"status={self.status!r}"
+        )
+
+class Redis:
     def __init__(self, num_nodes=5):
         self.num_nodes = num_nodes
+        self.threads = []
 
         self.nodes = [Node() for _ in range(self.num_nodes)]
         for node in self.nodes:
             node.peers = [n for n in self.nodes if n is not node]
 
         self.start()
-        self.status = Status.HEALTHY
+        self.status = NodeStatus.HEALTHY
 
     def start(self):
+        self.threads = [
+            threading.Thread(target=node.run) for node in self.nodes
+        ]
+        for thread in self.threads:
+            thread.start()
+
+    def stop(self):
         for node in self.nodes:
-            threading.Thread(target=node.run).start()
+            node.running = False
+
+        for thread in self.threads:
+            thread.join() 
+
+    def send(self, command):
+        for node in self.nodes:
+            node.receive_client_command(command)    
 
 class Node:
     def __init__(self):
         self.role = Role.FOLLOWER
-        self.kv = {}
-        self.status = Status.HEALTHY
+        self.status = NodeStatus.HEALTHY
+
+        # Log and peers
         self.log_index = 0
+        self.log = []
         self.peers = []
+        self.kv = {}
+
+        # Voting
         self.term = 1
         self.has_voted = False
         self.running = False
@@ -78,17 +113,45 @@ class Node:
                 self.role = Role.FOLLOWER
                 self.reset_election_timer()
 
+    def receive_client_command(self, command):
+        if self.role is not Role.LEADER:
+            return False
+
+        self.log.append(command)
+        # replicate entry to followers
+        # once a majority have the command, mark is committed
+        # each node applies the log entry
+        # once all nodes apply the entry, mark it applied
+    
+    def receive_leader_command(self, command):
+        pass
+
+    def get(self, command):
+        pass
+
+    def put(self, command):
+        pass
+
 class Role(Enum):
     LEADER = 1
     CANDIDATE = 2
     FOLLOWER = 3
 
-class Status(Enum):
+class NodeStatus(Enum):
     HEALTHY = 1
     DOWN = 2
     STALE = 3
     LAGGING = 4
     PARTITIONED = 5
+
+class LogStatus(Enum):
+    APPENDED = 1
+    COMMITTED = 2
+    APPLIED = 3
+
+class Op(Enum):
+    GET = 1
+    PUT = 2
 
 def send_vote_rpc(voter_node, candidate_term, candidate_log_index):
     if ((candidate_term > voter_node.term) or ((candidate_term == voter_node.term) and (candidate_log_index >= voter_node.log_index))) and not voter_node.has_voted:
